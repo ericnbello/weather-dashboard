4e03ce11508f4e4a296d21874e28dac9e0058e80
Eric Bello <ebello219@gmail.com>
2023-12-08 14:21:01 -0500

    updated static dir

--- enhanced_weather_app/settings.py
+++ enhanced_weather_app/settings.py
@@ -134,13 +134,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
+# STATIC_URL = 'enhanced_weather_app/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static/",
+# ]
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
