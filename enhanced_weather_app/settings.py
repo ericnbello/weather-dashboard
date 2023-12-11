cbe64fec210154d40d79b9ff09398f62bcdf7262
Eric Bello <ebello219@gmail.com>
2023-12-11 12:09:47 -0500

    updated static paths

--- enhanced_weather_app/settings.py
+++ enhanced_weather_app/settings.py
@@ -133,9 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static/' # Here
-STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'static/' # Here
+# STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
