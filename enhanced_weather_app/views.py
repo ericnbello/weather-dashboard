096d9154bd86b42a2c341d35497d88daeca0f31d
Eric Bello <ebello219@gmail.com>
2024-06-05 16:11:55 -0400

    changed all instances of httpx to requests

--- enhanced_weather_app/views.py
+++ enhanced_weather_app/views.py
@@ -40,7 +40,7 @@ def call_api(unit_system, location):
         lon = weather_data_1[0]["lon"]
 
         if unit_system == 'imperial':
-            r_2 = httpx.get('http://api.openweathermap.org/data/2.5/onecall?lat={0}&lon={1}&exclude=minutely&units={2}&appid={3}'.format(lat, lon, unit_system, openweather_api_key))
+            r_2 = requests.get('http://api.openweathermap.org/data/2.5/onecall?lat={0}&lon={1}&exclude=minutely&units={2}&appid={3}'.format(lat, lon, unit_system, openweather_api_key))
             degree_unit = '˚F'
             speed_unit = 'mph'
             pressure_unit  = 'mb'
@@ -49,7 +49,7 @@ def call_api(unit_system, location):
             # next_unit_system = 'metric'
             # current_unit_system = next_unit_system
         else:
-            r_2 = httpx.get('http://api.openweathermap.org/data/2.5/onecall?lat={0}&lon={1}&exclude=minutely&units=metric&appid={3}'.format(lat, lon, openweather_api_key))
+            r_2 = requests.get('http://api.openweathermap.org/data/2.5/onecall?lat={0}&lon={1}&exclude=minutely&units=metric&appid={3}'.format(lat, lon, openweather_api_key))
             degree_unit = '˚C'
             speed_unit = 'km/h'
             pressure_unit = 'mb'
@@ -58,7 +58,7 @@ def call_api(unit_system, location):
             # next_unit_system = 'imperial'
             # current_unit_system = next_unit_system
 
-        r_3 = httpx.get('http://api.openweathermap.org/data/2.5/air_pollution?lat={0}&lon={1}&appid={2}'.format(lat, lon, openweather_api_key))
+        r_3 = requests.get('http://api.openweathermap.org/data/2.5/air_pollution?lat={0}&lon={1}&appid={2}'.format(lat, lon, openweather_api_key))
 
         # Full API response data
         # weather_data_2 = json.loads(r_2.content)
